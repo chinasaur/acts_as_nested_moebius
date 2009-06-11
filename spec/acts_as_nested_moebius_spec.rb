657ed5e9f764644de6294947284d9e3ceb621d06
Chinasaur <chinasaurli@gmail.com>
2009-06-11 00:17:00 -0400

    Added ActiveRecord with SQLite in memory to tests!

--- spec/acts_as_nested_moebius_spec.rb
+++ spec/acts_as_nested_moebius_spec.rb
@@ -1,1 +1,17 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
+
+# Thanks!: 
+# http://stackoverflow.com/questions/722918/testing-ruby-gems-under-rails
+require 'rubygems'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :dbfile => ':memory:')
+
+ActiveRecord::Schema.define(:version => 1) do
+  create_table :nodes do |t|
+    t.string :name
+  end
+end
+
+class Node < ActiveRecord::Base
+end
